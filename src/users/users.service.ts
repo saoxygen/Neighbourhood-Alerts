8f7e1d522b04a79c46c61c4a5c3983d47885641a
Kopano Sekonyela <kopanodrkk@icloud.com>
2026-02-25 18:01:27 +0200

    Added CRUD operations that connect to my local PostGres DB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>){}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(uuid: string) {
+    return this.userRepository.findOne({where: {uuid}});
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(uuid: string, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update({uuid: uuid}, {name: updateUserDto.name});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(uuid: string) {
+    return this.userRepository.delete({ uuid: uuid });
   }
 }
